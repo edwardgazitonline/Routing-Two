0defb825747867ee939246187d4d693701ade293
edwardgazitonline <75829088+edwardgazitonline@users.noreply.github.com>
2023-04-30 11:57:35 +0300

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import {Routes,RouterModule } from '@angular/router';
-import { RouterUpgradeInitializer } from '@angular/router/upgrade';
 import { AppComponent } from './app.component'
 import { HeroComponent } from './hero/hero.component';
 import { heroesComponent } from './heroes/heroes.component';
