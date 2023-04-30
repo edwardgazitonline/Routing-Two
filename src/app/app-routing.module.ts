574d6dcaf3ff9b5c7367a4be4df7a55d46fb81b7
edwardgazitonline <75829088+edwardgazitonline@users.noreply.github.com>
2023-04-30 11:18:04 +0300

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import {Routes,RouterModule } from '@angular/router';
 import { RouterUpgradeInitializer } from '@angular/router/upgrade';
 import { AppComponent } from './app.component'
+import { HeroComponent } from './hero/hero.component';
+import { heroesComponent } from './heroes/heroes.component';
 
-const routes:Routes = [];
+const routes:Routes = [
+{path: 'heroes' , component: heroesComponent},
+{path :'hero' , component: HeroComponent }
+
+
+];
 
 @NgModule({
 imports:[RouterModule.forRoot(routes)],
