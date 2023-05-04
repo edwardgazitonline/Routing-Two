a154db0d0b6ca61eff7c83ced868ebeeed5ab372
edwardgazitonline <75829088+edwardgazitonline@users.noreply.github.com>
2023-05-04 07:11:15 +0300

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
-import {Routes,RouterModule } from '@angular/router';
+import { Routes , RouterModule } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { heroesComponent } from './heroes/heroes.component';
 
 const routes:Routes = [
-{path: 'heroes' , component: heroesComponent},
+{path: 'heroes' , component: heroesComponent },
 {path :'hero' , component: HeroComponent }
-
-
 ];
 
 @NgModule({
